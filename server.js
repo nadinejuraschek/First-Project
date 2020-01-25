36a3c2683f15cfddb749a7755bc7d1dee85bce6d
Nadine Ju <nadine.jura@online.de>
2020-01-24 21:44:44 -0800

    passport logic and routes added to server.js

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 require('dotenv').config();
 const   express         = require('express'),
         mongoose        = require('mongoose'),
+        passport        = require('passport'),
+        LocalStrategy   = require('passport-local'),
         ejs             = require('ejs'),
-        app             = express();
+        app             = express(),
+        User            = require('./models/user');
 
 // MONGODB
 mongoose.connect(process.env.MONGO_DB);
@@ -19,9 +22,81 @@ app.set('view engine', 'ejs');
 
 // SCHEMA SETUP
 
+// PASSPORT CONFIG
+app.use(require('express-session')({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false 
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // ============================
 // SERVER
 // ============================
+// redirect user to /login or /home when visiting /
+app.get('/', isLoggedIn, function(req, res){
+    res.redirect('/home');
+});
+
+// handle sign up logic
+app.get('/signup', function(req, res){
+    res.render('signup', {title: 'Register'});
+});
+app.post('/signup', function(req, res){
+    const newUser = new User({
+        email: req.body.email,
+        username: req.body.username
+    });
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log("Something went wrong: " + err);
+            return res.render('signup');
+        }
+        passport.authenticate('local')(req, res, function(){
+            console.log('User is logged in.');
+            res.redirect('/home');
+        })
+    });
+});
+
+// login logic
+app.get('/login', function(req, res){
+    res.render('log-in', {title: 'Login'});
+});
+app.post('/login', passport.authenticate('local', 
+    { 
+        successRedirect: '/home',
+        failureRedirect: '/login'
+}));
+
+// logout logic
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/login');
+});
+
+// render /home, /overview, and /discover if user is signed in, otherwise redirect to /login
+app.get('/home', isLoggedIn, function(req, res){
+    res.render('home', {title: 'Home', currentUser: req.user.username});
+});
+app.get('/overview', isLoggedIn, function(req, res){
+    res.render('overview', {title: 'Overview', currentUser: req.user.username});
+});
+app.get('/discover', isLoggedIn, function(req, res){
+    res.render('discover', {title: 'Discover', currentUser: req.user.username});
+});
+
+// check if user is logged in logic
+function isLoggedIn(req, res, next){
+    if (req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
 
 // catch all route
 app.get('*', function(req, res){
